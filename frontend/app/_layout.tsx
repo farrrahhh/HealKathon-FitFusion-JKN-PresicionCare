15cde00bc9a686d56e58c3a2bbf6d1ca0032068e
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-02 13:28:30 +0700

    backend

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,27 +1,11 @@
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
 import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    'SpaceMono-Regular': require('@/assets/fonts/SpaceMono-Regular.ttf'),
-    'Poppins-Regular': require('@/assets/fonts/Poppins Regular 400.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
-
+export default function HomeLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="details" />
+      <Stack.Screen name="settings" />
     </Stack>
   );
 }
